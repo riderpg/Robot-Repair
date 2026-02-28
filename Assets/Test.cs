badc134d344da68bbc42eb085ab7d2cdba3efc21
Sumin Cho <sumin.cho@unity3d.com>
2026-02-28 14:57:54 +0900

    Add project files

---
+++ Assets/Test.cs
@@ -0,0 +1,13 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public event Action OnAction;
+
+    private void Start()
+    {
+        
+    }
+}
